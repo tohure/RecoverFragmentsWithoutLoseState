1621b59ef482a5b3da4b97a94252b0813b00b0e4
Carlo Huamán <carlohuaman@bcp.com.pe>
2020-08-24 01:00:31 -0500

    refactor(app): migrate to androidX, bugfix with first position, add onlive log

--- app/src/main/java/io/tohure/changefragmentstest/MainActivity.kt
+++ app/src/main/java/io/tohure/changefragmentstest/MainActivity.kt
@@ -1,9 +1,9 @@
 package io.tohure.changefragmentstest
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG_ONE = "first"
@@ -13,8 +13,6 @@ private const val TAG_FOURTH = "fourth"
 private const val MAX_HISTORIC = 5
 
 class MainActivity : AppCompatActivity() {
-
-    private var currentFragment: Fragment? = null
 
     private val listState = mutableListOf<StateFragment>()
     private var currentTag: String = TAG_ONE
@@ -30,119 +28,114 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-
         navigation.setOnNavigationItemSelectedListener { menuItem ->
 
             if (currentMenuItemId != menuItem.itemId) {
-
-                val fragment: Fragment
-                oldTag = currentTag
-
                 currentMenuItemId = menuItem.itemId
 
                 when (currentMenuItemId) {
-                    R.id.navigation_shop -> {
-                        currentTag = TAG_ONE
-                        fragment = FirstFragment.newInstance()
-                        loadFragment(fragment, currentTag)
-                    }
-                    R.id.navigation_gifts -> {
-                        currentTag = TAG_SECOND
-                        fragment = SecondFragment.newInstance()
-                        loadFragment(fragment, currentTag)
-                    }
-                    R.id.navigation_cart -> {
-                        currentTag = TAG_THIRD
-                        fragment = ThirdFragment.newInstance()
-                        loadFragment(fragment, currentTag)
-                    }
-                    R.id.navigation_profile -> {
-                        currentTag = TAG_FOURTH
-                        fragment = FourthFragment.newInstance()
-                        loadFragment(fragment, currentTag)
-                    }
+                    R.id.navigation_shop -> changeFragment(TAG_ONE, FirstFragment.newInstance())
+                    R.id.navigation_gifts -> changeFragment(TAG_SECOND, SecondFragment.newInstance())
+                    R.id.navigation_cart -> changeFragment(TAG_THIRD, ThirdFragment.newInstance())
+                    R.id.navigation_profile -> changeFragment(TAG_FOURTH, FourthFragment.newInstance())
                 }
 
                 return@setOnNavigationItemSelectedListener true
-
             }
 
             false
         }
+    }
 
+    private fun changeFragment(tagToChange: String, fragment: Fragment) {
+        if (currentTag != tagToChange) {
+            val ft = supportFragmentManager.beginTransaction()
+            val currentFragment = supportFragmentManager.findFragmentByTag(currentTag)
+            val fragmentToReplaceByTag = supportFragmentManager.findFragmentByTag(tagToChange)
+
+            oldTag = currentTag
+            currentTag = tagToChange
+
+            if (fragmentToReplaceByTag != null) {
+                currentFragment?.let { ft.hide(it).show(fragmentToReplaceByTag) }
+            } else {
+                currentFragment?.let { ft.hide(it).add(R.id.frame_container, fragment, tagToChange) }
+            }
+
+            ft.commit()
+
+            addBackStack()
+        }
     }
 
     override fun onBackPressed() {
-
         if (listState.size >= 1) {
             recoverFragment()
         } else {
             super.onBackPressed()
         }
-
     }
 
     private fun recoverFragment() {
 
         val lastState = listState.last()
-        listState.removeAt(listState.size - 1)
-
-        currentTag = lastState.currentFragmentTag
-        oldTag = lastState.oldFragmentTag
-
-        Log.d("thr recover", "$currentTag - $oldTag")
 
         val ft = supportFragmentManager.beginTransaction()
 
-        val currentFragment = supportFragmentManager.findFragmentByTag(currentTag)
-        val oldFragment = supportFragmentManager.findFragmentByTag(oldTag)
+        val currentFragmentByTag = supportFragmentManager.findFragmentByTag(lastState.currentFragmentTag)
+        val oldFragmentByTag = supportFragmentManager.findFragmentByTag(lastState.oldFragmentTag)
 
-        if (currentFragment.isVisible && oldFragment.isHidden) {
-            ft.hide(currentFragment).show(oldFragment)
+        if ((currentFragmentByTag != null && currentFragmentByTag.isVisible) &&
+                (oldFragmentByTag != null && oldFragmentByTag.isHidden)) {
+            ft.hide(currentFragmentByTag).show(oldFragmentByTag)
         }
 
         ft.commit()
 
         val menu = navigation.menu
 
-        when (oldTag) {
-            TAG_ONE -> menu.getItem(0).isChecked = true
-            TAG_SECOND -> menu.getItem(1).isChecked = true
-            TAG_THIRD -> menu.getItem(2).isChecked = true
-            TAG_FOURTH -> menu.getItem(3).isChecked = true
+        when (lastState.oldFragmentTag) {
+            TAG_ONE -> setMenuItem(menu.getItem(0))
+            TAG_SECOND -> setMenuItem(menu.getItem(1))
+            TAG_THIRD -> setMenuItem(menu.getItem(2))
+            TAG_FOURTH -> setMenuItem(menu.getItem(3))
         }
 
+        //Remove from Stack
+        listState.removeLast()
+
+        if (listState.isEmpty()) {
+            currentTag = TAG_ONE
+            oldTag = TAG_ONE
+        } else {
+            currentTag = listState.last().currentFragmentTag
+            oldTag = listState.last().oldFragmentTag
+        }
+        updateLog()
+    }
+
+    private fun updateLog() {
+        tvCurrentTag.text = currentTag
+        tvOldTag.text = oldTag
+    }
+
+    private fun setMenuItem(menuItem: MenuItem) {
+        menuItem.isChecked = true
+        currentMenuItemId = menuItem.itemId
     }
 
     private fun loadFirstFragment() {
         val transaction = supportFragmentManager.beginTransaction()
-        currentFragment = FirstFragment.newInstance()
-        transaction.add(R.id.frame_container, currentFragment, TAG_ONE)
+        transaction.add(
+                R.id.frame_container,
+                FirstFragment.newInstance(),
+                TAG_ONE)
         transaction.commit()
-    }
-
-    private fun loadFragment(fragment: Fragment, tag: String) {
-
-        if (currentFragment !== fragment) {
-            val ft = supportFragmentManager.beginTransaction()
-
-            if (fragment.isAdded) {
-                ft.hide(currentFragment).show(fragment)
-            } else {
-                ft.hide(currentFragment).add(R.id.frame_container, fragment, tag)
-            }
-            currentFragment = fragment
-
-            ft.commit()
-
-            addBackStack()
-        }
-
     }
 
     //Like YouTube
     private fun addBackStack() {
-        Log.d("thr add", "$currentTag - $oldTag")
+        updateLog()
 
         when (listState.size) {
             MAX_HISTORIC -> {
@@ -163,7 +156,5 @@ class MainActivity : AppCompatActivity() {
                 listState.add(StateFragment(currentTag, oldTag))
             }
         }
-
     }
-
 }
